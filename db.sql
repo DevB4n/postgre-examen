23a858c50a60c99387eaa294f6f1973e62be2fd5
DevB4n <buenochaconesteban@gmail.com>
2025-08-21 07:38:57 -0500

    feat: :card_file_box: y SQLazo bb?uwu

--- db.sql
+++ db.sql
@@ -25,3 +25,32 @@ CREATE TABLE compra_productos(
     ADD CONSTRAINT fk_id_compra FOREIGN KEY (id_compra) REFERENCES compra(id_compra),
     ADD CONSTRAINT fk_id_producto FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
+DROP TABLE IF EXISTS productos;
+CREATE TABLE productos(
+    id_producto SERIAL PRIMARY KEY,
+    id_categoria INT(14),
+    nombre_producto VARCHAR(50),
+    valor_unitario DECIMAL(30,2),
+    stock INT(10),
+    ADD CONSTRAINT fk_id_categoria FOREIGN KEY (id_categoria) REFERENCES categoria_producto(id_categoria)
+);
+DROP TABLE IF EXISTS proveedores_productos;
+CREATE TABLE proveedores_productos(
+    id_proveedor INT(14),
+    id_producto INT(14),
+    ADD CONSTRAINT fk_id_proveedor FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor),
+    ADD CONSTRAINT fk_id_producto FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+);
+DROP TABLE IF EXISTS categoria_producto;
+CREATE TABLE categoria_producto(
+    id_categoria SERIAL PRIMARY KEY,
+    nombre VARCHAR(50)
+);
+DROP TABLE IF EXISTS proveedores;
+CREATE TABLE proveedores(
+    id_proveedor SERIAL PRIMARY KEY,
+    nombre VARCHAR(50),
+    direccion VARCHAR(50),
+    correo_electronico VARCHAR(50)
+)
+
