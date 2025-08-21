ccaaba239daab0feea1d11c6adb356bd100f67d9
DevB4n <buenochaconesteban@gmail.com>
2025-08-21 07:26:04 -0500

    feat: :card_file_box: db

--- db.sql
+++ db.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS clientes;
 CREATE TABLE clientes(
     id_cliente SERIAL PRIMARY KEY,
     nombre VARCHAR(30),
@@ -5,4 +6,22 @@ CREATE TABLE clientes(
     telefono INT(20),
     correo_electronico VARCHAR(50),
     direccion VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS compra
+CREATE TYPE metodo ENUM('tarjeta','efectivo')
+CREATE TABLE compra(
+    id_compra SERIAL PRIMARY KEY,
+    id_cliente INT(14),
+    fecha_compra TIMESTAMP,
+    total INT(14),
+    metodo metodo
+);
+
+DROP TABLE IF EXISTS compra_productos;
+CREATE TABLE compra_productos(
+    id_compra INT(14),
+    id_producto INT(14),
+    ADD CONSTRAINT fk_id_compra FOREIGN KEY (id_compra) REFERENCES compra(id_compra),
+    ADD CONSTRAINT fk_id_producto FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
